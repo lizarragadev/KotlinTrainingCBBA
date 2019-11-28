d89eea9b0164cd0f8663324974555de115eb49fd
Gustavo <lizarraga.dev@gmail.com>
2019-11-28 12:07:43 -0400

    Base code PokeKotlin

---
+++ 02_PokeKotlin/app/src/main/java/com/miramicodigo/pokekotlin/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.miramicodigo.pokekotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.miramicodigo.pokekotlin.adapter.PokemonAdapter
+import com.miramicodigo.pokekotlin.model.PokemonResponse
+import com.miramicodigo.pokekotlin.service.PokeInterface
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var retrofit: Retrofit
+    lateinit var listaPokemonAdapter: PokemonAdapter
+    var cantidadLote = 0
+    var sePuedeCargar = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+
+    private fun obtenerDatos(offset: Int) {
+
+    }
+
+}
---
+++ 02_PokeKotlin/app/src/main/java/com/miramicodigo/pokekotlin/service/PokeInterface.kt
@@ -0,0 +1,11 @@
+package com.miramicodigo.pokekotlin.service
+
+import com.miramicodigo.pokekotlin.model.PokemonResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface PokeInterface {
+
+
+}
