fe8fc260e975bad017606f224898147307ae42ea
Gustavo <lizarraga.dev@gmail.com>
2019-11-28 12:07:52 -0400

    Base code SoundPool

---
+++ 03_SoundPool/app/src/main/java/com/miramicodigo/elregalodebart/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.miramicodigo.elregalodebart
+
+import android.os.Bundle
+import android.media.SoundPool
+import android.media.AudioAttributes
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+lateinit var spIdiota: SoundPool
+lateinit var spCallate: SoundPool
+lateinit var spVeteAlDiablo: SoundPool
+var resIdiota = 0
+var resCallate = 0
+var resVeteAlDiablo = 0
+
+open class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
+
+        createSoundPool()
+    }
+
+    private fun createSoundPool() {
+        val attributes = AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_GAME).setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION).build()
+        spIdiota = SoundPool.Builder().setAudioAttributes(attributes).build()
+        spCallate = SoundPool.Builder().setAudioAttributes(attributes).build()
+        spVeteAlDiablo = SoundPool.Builder().setAudioAttributes(attributes).build()
+        loadSoundPool()
+    }
+
+    fun loadSoundPool() {
+
+    }
+
+    fun idiota(v: View) {
+
+    }
+
+    fun callate(v: View) {
+
+    }
+
+    fun veteAlDiablo(v: View) {
+
+    }
+
+}
